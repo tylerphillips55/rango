cb1578e25b7b7ae6caccd53fca59860c58c6c79f
Tyler Phillips <phillipst55@gmail.com>
2016-08-10 20:58:03 -0400

    Changed rango views

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ]
