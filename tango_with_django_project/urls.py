ff4dece0f4303b3e924d41ed89b95e67e85e1fdb
Tyler Phillips <phillipst55@gmail.com>
2016-08-11 17:19:20 -0400

    Added templates and updated views

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-from rango import views
 
 urlpatterns = [
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
